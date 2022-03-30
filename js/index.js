c814a308635445d43d0d48205f9715776e7cfa19
LilleAndersen <71332089+LilleAndersen@users.noreply.github.com>
2022-03-30 10:42:39 +0200

    so, the last time i fucked your mother, i actually experienced some shitting on the toilet, and i dont know how to feel about it. anyway bye guys

--- js/index.js
+++ js/index.js
@@ -1,25 +1,48 @@
+let currentChatroom;
+let frmfcs;
+
+currentChatroom = localStorage['currentChatroom'] || '/js/chatroom-alpha.json';
+
 //axios.post('URL', data, {
 //    withCredentials: true
 //});
 
-// Dynamically loads content
-fetch("/js/testchat.json")
-    .then(response => response.json())
-    .then(data =>
-    {
-        for (let i = 0; i < data.messages.length; i++)
+
+// onload= function() {
+//     frmfcs = document.getElementById('focus');
+//     frmfcs.focus();
+// }
+
+// let element = document.getElementById('focus');
+// element.scrollTop = element.offsetHeight
+
+
+loadChat()
+
+function loadChat() {
+    document.querySelector("#chat-list").innerHTML = ` `
+
+    // Dynamically loads content
+    fetch("/js/" + currentChatroom + ".json")
+        .then(response => response.json())
+        .then(data =>
         {
-            document.querySelector("#chat-list").innerHTML += `
+            for (let i = 0; i < data.messages.length; i++)
+            {
+                document.querySelector("#chat-list").innerHTML += `
                 <div class="chat">
-                    <img src="${data.messages[i].chatterimage}" alt="skootskoot">
-                    <div class="author-message-box">
+                    <div class="chat-user">
+                        <img src="${data.messages[i].chatterimage}" alt="skootskoot">
                         <p class="bold">${data.messages[i].author}</p>
-                        <p>${data.messages[i].message}</p>
-                    </div>
+                        </div>
+                    <p class="chat-message">${data.messages[i].message}</p>
                 </div>
             `;
-        }
-    });
+            }
+
+            scroll().then()
+        });
+}
 
 // Dynamically loads content
 fetch("/js/testchatrooms.json")
@@ -35,39 +58,54 @@ fetch("/js/testchatrooms.json")
                 </div>
             `;
         }
+
+        changeChatRoom(currentChatroom)
     });
 
-// const ws = new WebSocket("ws://10.80.18.182:14242/anything");
-//
-// ws.onclose = () => console.log("yo mom");
-//
-// ws.onopen = () =>
-// {
-//     console.log("hi there");
-//     ws.send(JSON.stringify({
-//         "author": "little",
-//         "data": "soni is not cool"
-//     }));
-//     return false;
-// }
-//
-// ws.onmessage = e =>
-// {
-//     console.log(JSON.parse(e.data));
-//     return false;
-// }
+let chatroom = "chatroom-alpha";
 
-let chatroomname = "a"
+function changeChatRoom(chatroomId) {
 
-function changeChatRoom(chatroom) {
+    //oldchatroomname = chatroom || "chatroom-alpha";
 
-    document.querySelector("#" + chatroomname).style.backgroundColor = "#40446e";
+    document.querySelector("#" + chatroom).style.backgroundColor = "#40446e";
 
-    console.log("your mco")
+    document.querySelector("#" + chatroomId).style.backgroundColor = "#123";
+    // document.querySelector("#" + chatroom).classList.add =
 
-    document.querySelector("#" + chatroom).style.backgroundColor = "#123";
-    document.querySelector("#" + chatroom).classList.add =
+    chatroom = chatroomId
 
+    currentChatroom = chatroomId
 
-    chatroomname = chatroom
+    localStorage['currentChatroom'] = currentChatroom
+
+    loadChat()
 }
+
+async function scroll() {
+    let myDiv = document.querySelector("#chat-list");
+    myDiv.scrollTop = myDiv.scrollHeight;
+}
+
+///////////////////////////////////////////////////////////////////////
+const ws = new WebSocket("ws://10.80.18.182:14242/anything");
+
+ws.onclose = e => console.log("closed", e);
+
+ws.onopen = e =>
+{
+    console.log("open", e);
+    ws.send(JSON.stringify({
+        "author": "little",
+        "data": "soni is not cool"
+    }));
+    return false;
+}
+
+ws.onmessage = e =>
+{
+    console.log("message", e);
+    return false;
+}
+
+ws.onerror = e => console.log("error", e);
