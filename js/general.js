9b742eb925941a2dce4079c97dae4776ec8db8a7
LilleAndersen <71332089+LilleAndersen@users.noreply.github.com>
2022-04-27 10:40:54 +0200

    fix authentication, logging in and getting auth token, started fixing settings page

--- js/general.js
+++ js/general.js
@@ -11,17 +11,22 @@ const api = "https://api.social.recalstudios.net/";
 // Gets auth token with credentials
 async function getAuthToken()
 {
+    // username = document.querySelector("#username").value.toString();
+    // passphrase = document.querySelector("#passphrase").value.toString();
+
     try
     {
         // API request with axios. Post request where the url field describes where the request should go and the data field what should be in it.
         authToken = (await axios({
             method: 'post',
-            url: api + 'auth/token/renew',
+            url: api + 'auth/token/new',
             data: {
                 username: username,
                 password: passphrase
             }
         })).data;
+
+        localStorage['token'] = authToken;
     }
     catch (e)
     {
@@ -42,10 +47,15 @@ async function getUser() {
     user = (await axios({
         method: 'post',
         url: api + 'user/user',
-        data: {
+        headers: {
             Authorization: 'Bearer ' + authToken
         }
     })).data;
 
     console.log(user)
+}
+
+async function checkIfAuthTokenExpired()
+{
+
 }
