49b1aaa6ab649b14dee7a1347ef804f7cfe9c084
Soni <soni@recalstudios.net>
2023-11-14 13:09:26 +0100

    feat: finish passphrase reset form

--- js/reset-passphrase.js
+++ js/reset-passphrase.js
@@ -1,3 +1,11 @@
+// Get the URL parameters
+const urlParams = new URLSearchParams(window.location.search);
+const resetToken = urlParams.get('resetToken');
+
+// A resetToken was provided. This means that the user is navigating to the page after receiving the reset email
+if (resetToken) document.querySelector("#new-passphrase-form").classList.remove("hidden");
+else document.querySelector("#form").classList.remove("hidden");
+
 async function resetPassphrase()
 {
     if (document.querySelector("#email").value.length === 0) openDialog("missing-user-info");
@@ -6,7 +14,7 @@ async function resetPassphrase()
         // Gets values of email field
         const email = document.querySelector("#email").value.toString();
 
-        // Gets user from API
+        // Send data to API
         axios({
             method: 'post',
             url: api + 'user/reset-passphrase',
@@ -14,5 +22,30 @@ async function resetPassphrase()
                 email: email
             }
         });
+    }
+}
+
+async function confirmNewPassphrase()
+{
+    if (document.querySelector("#passphrase").value.length === 0 || document.querySelector("#confirm-passphrase").value.length === 0) openDialog("missing-user-info");
+    else
+    {
+        // Get values
+        const passphrase = document.querySelector("#passphrase").value.toString();
+        const passphraseConfirm = document.querySelector("#confirm-passphrase").value.toString();
+
+        if (passphrase !== passphraseConfirm) openDialog("passphrases-must-match");
+        else
+        {
+            // Send data to API
+            axios({
+                method: 'post',
+                url: api + 'user/set-passphrase',
+                data: {
+                    resetToken: resetToken,
+                    passphrase: passphrase
+                }
+            });
+        }
     }
 }
